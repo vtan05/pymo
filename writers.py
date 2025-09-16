ed33bb5739554e8f8f5031d94b0cc88bf073148c
Vanessa Tan <vanessa.tan@eee.upd.edu.ph>
2025-09-16 06:06:20 +0000

    revise bvh writer to include only root pos

--- writers.py
+++ writers.py
@@ -53,7 +53,9 @@ class BVHWriter():
         #print("rot_order = " + rot_order)
         channels = X.skeleton[joint]['channels']
         rot = [c for c in channels if ('rotation' in c)]
-        pos = [c for c in channels if ('position' in c)]
+        # pos = [c for c in channels if ('position' in c)]
+        # Only allow position channels on the root
+        pos = [c for c in channels if 'position' in c] if joint == X.root_name else []
         
         n_channels = len(rot) +len(pos)
         ch_str = ''
@@ -74,3 +76,50 @@ class BVHWriter():
                 self._printJoint(X, c, tab+1, ofile)
 
         ofile.write('%s}\n'%('\t'*(tab)))
+
+    # def _printJoint(self, X, joint, tab, ofile):
+    #     # Node header
+    #     if X.skeleton[joint]['parent'] is None:
+    #         ofile.write(f'ROOT {joint}\n')
+    #     elif len(X.skeleton[joint]['children']) > 0:
+    #         ofile.write(f'{"\t"*tab}JOINT {joint}\n')
+    #     else:
+    #         ofile.write(f'{"\t"*tab}End Site\n')
+
+    #     ofile.write(f'{"\t"*tab}' + '{\n')
+    #     ox, oy, oz = X.skeleton[joint]['offsets']
+    #     ofile.write(f'{"\t"*(tab+1)}OFFSET {ox:.5f} {oy:.5f} {oz:.5f}\n')
+
+    #     rot_order = X.skeleton[joint]['order']          # e.g., "XYZ"
+    #     channels   = X.skeleton[joint]['channels']      # e.g., ['Xposition','Yposition','Zposition','Zrotation','Xrotation','Yrotation']
+
+    #     # Only allow position channels on the root
+    #     pos = [c for c in channels if 'position' in c] if joint == X.root_name else []
+    #     # Rotation channels always allowed
+    #     rot = [c for c in channels if 'rotation' in c]
+
+    #     ch_names_written = []
+
+    #     # Add position channels that actually exist in X.values
+    #     for cn in pos:
+    #         col = f'{joint}_{cn}'
+    #         if col in X.values.columns:
+    #             self.motions_.append(np.asarray(X.values[col].values))
+    #             ch_names_written.append(cn)
+
+    #     # Add rotation channels following rot_order, but only if they exist
+    #     for axis in rot_order:
+    #         cn = f'{axis}rotation'
+    #         col = f'{joint}_{cn}'
+    #         if cn in rot and col in X.values.columns:
+    #             self.motions_.append(np.asarray(X.values[col].values))
+    #             ch_names_written.append(cn)
+
+    #     # Write CHANNELS only for non-End Site nodes
+    #     if len(X.skeleton[joint]['children']) > 0 or X.skeleton[joint]['parent'] is None:
+    #         ofile.write(f'{"\t"*(tab+1)}CHANNELS {len(ch_names_written)}' + ''.join(f' {c}' for c in ch_names_written) + '\n')
+    #         # Recurse into children
+    #         for c in X.skeleton[joint]['children']:
+    #             self._printJoint(X, c, tab+1, ofile)
+
+    #     ofile.write(f'{"\t"*tab}' + '}\n')
